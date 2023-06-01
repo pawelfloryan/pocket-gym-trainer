d0bbd5e27ea3b8dbfb138059d1066acb450a4e78
OreoPL-studio <pawel.floryan@interia.pl>
2023-06-01 23:42:43 +0200

    Login page design finished for now

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -18,6 +18,8 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
     final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+    double deviceWidth(BuildContext context) =>
+        MediaQuery.of(context).size.width;
 
     return Scaffold(
         key: _scaffoldKey,
@@ -56,7 +58,11 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
                 TextFormField(
-                  decoration: InputDecoration(labelText: "Enter your password"),
+                  obscureText: true,
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    labelText: "Enter your password",
+                    ),
                   validator: (value) {
                     if (value!.isEmpty) {
                       return "Enter correct password";
@@ -65,51 +71,77 @@ class _LoginPageState extends State<LoginPage> {
                     }
                   },
                 ),
-                Container(
-                  margin: const EdgeInsets.only(
-                      left: 170, top: 50, right: 0, bottom: 0),
-                  child: GestureDetector(
-                  //  onPressed: () {
-                  //    if (formKey.currentState!.validate()) {}
-                  //  },
-                  //  child: Text(
-                  //    "SUBMIT",
-                  //    style: const TextStyle(fontSize: 70),
-                  //  ),
-                    onTap: () {
-                      setState(() {
-                        isElevated = !isElevated;
-                      });
-                    },
-                    child: AnimatedContainer(
-                      duration: Duration(milliseconds: 200),
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(35),
-                        boxShadow: isElevated ? 
-                        [
-                          BoxShadow(
-                            color: Colors.grey[500]!,
-                            offset: Offset(4,4),
-                            blurRadius: 15,
-                            spreadRadius: 1
-                          ),
-                          BoxShadow(
-                            color: Colors.white,
-                            offset: Offset(-4,-4),
-                            blurRadius: 15,
-                            spreadRadius: 1
-                          ),
-                        ] : null,
-                      ),
-                      child: Icon(
-                        Icons.arrow_forward_ios,
-                        size: 50,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(
+                          left: 0, top: 0, right: 40, bottom: 0),
+                      child: Text(
+                        "Haven't made \nan account yet?",
+                        style: TextStyle(fontSize: 17),
                         ),
                     ),
-                  ),
+                    Container(
+                      //MediaQuery.of(context).size.width
+                      margin: EdgeInsets.only(
+                        left: 0,
+                        top: 50,
+                        right: 0,
+                        bottom: 0,
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: GestureDetector(
+                          //  onPressed: () {
+                          //    if (formKey.currentState!.validate()) {}
+                          //  },
+                          //  child: Text(
+                          //    "SUBMIT",
+                          //    style: const TextStyle(fontSize: 70),
+                          //  ),
+                          onTap: () {
+                            setState(() {
+                              isElevated = !isElevated;
+                            });
+                            if (formKey.currentState!.validate()) {
+                              final snackBar =
+                                  SnackBar(content: Text('Submitting form...'));
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            }
+                          },
+                          child: AnimatedContainer(
+                            duration: Duration(milliseconds: 200),
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(35),
+                              boxShadow: isElevated
+                                  ? [
+                                      BoxShadow(
+                                          color: Colors.grey[500]!,
+                                          offset: Offset(4, 4),
+                                          blurRadius: 15,
+                                          spreadRadius: 1),
+                                      BoxShadow(
+                                          color: Colors.white,
+                                          offset: Offset(-4, -4),
+                                          blurRadius: 15,
+                                          spreadRadius: 1),
+                                    ]
+                                  : null,
+                            ),
+                            child: Icon(
+                              Icons.arrow_forward,
+                              size: 75,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
