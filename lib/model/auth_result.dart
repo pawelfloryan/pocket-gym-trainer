bec01f9c2e6cc85f1218b499674a0f42e789dda5
OreoPL-studio <81591050+pawelfloryan@users.noreply.github.com>
2023-06-06 12:18:51 +0200

    Auth result model updated

--- lib/model/auth_result.dart
+++ lib/model/auth_result.dart
@@ -3,26 +3,29 @@ import 'dart:ffi';
 
 class AuthResult {
   late String? token;
+  late String? refreshToken;
   late bool? result;
   late List<dynamic>? errors;
 
   AuthResult({
     this.token,
+    this.refreshToken,
     this.result,
     this.errors,
   });
 
   Map<String, Object?> toJson() => {
         "token": token,
+        "refreshToken": refreshToken,
         "result": result,
         "errors": errors,
       };
 
   static AuthResult fromJson(Map<String, dynamic> json) => AuthResult(
-        token: json["token"] as String?,
-        result: json["result"] as bool,
-        errors: json["errors"] as List<dynamic>?
-      );
+      token: json["token"] as String?,
+      refreshToken: json["refreshToken"] as String?,
+      result: json["result"] as bool,
+      errors: json["errors"] as List<dynamic>?);
 }
 
 List<AuthResult> authResultFromJsonList(String str) =>
@@ -33,4 +36,5 @@ Map<String, dynamic> authResultFromJson(String str) {
   return jsonMap;
 }
 
-String authResultToJson(AuthResult authResult) => json.encode(authResult.toJson());
+String authResultToJson(AuthResult authResult) =>
+    json.encode(authResult.toJson());
