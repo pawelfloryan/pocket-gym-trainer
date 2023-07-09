54a9a8929b6297919a85060d1aa98be4b40a81d9
OreoPL-studio <pawel.floryan@interia.pl>
2023-07-09 13:47:25 +0200

    Counter works + more

--- lib/components/workout_counter.dart
+++ lib/components/workout_counter.dart
@@ -1,32 +1,74 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class WorkoutCounter extends StatelessWidget {
-  final ValueListenable<int> number;
-
-  WorkoutCounter(this.number);
+class WorkoutCounter extends StatefulWidget {
+  static late ValueNotifier<int> number = ValueNotifier<int>(0);
 
   @override
-  Widget build(BuildContext context) {
-    int clickCount =  number.value; 
+  State<WorkoutCounter> createState() => _WorkoutCounterState();
+}
 
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.all(45.0),
-        padding: const EdgeInsets.all(10.0),
-        width: double.infinity,
-        height: 180,
-        child: FloatingActionButton(
-          onPressed: () {},
-          child: Text(
-            "$clickCount",
-            style: clickCount < 100
-                ? const TextStyle(fontSize: 100)
-                : clickCount < 1000
-                    ? const TextStyle(fontSize: 75)
-                    : const TextStyle(fontSize: 50),
+class _WorkoutCounterState extends State<WorkoutCounter> {
+  bool controls = false;
+  
+  @override
+  Widget build(BuildContext context) {
+    int clickCount = WorkoutCounter.number.value;
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        children: [
+          Visibility(
+            visible: controls,
+            child: Container(
+              child: FloatingActionButton(
+                onPressed: () {
+                  //TODO
+                },
+                child: Icon(Icons.add),
+              ),
+            ),
           ),
-        ),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.only(top: 45, bottom: 45),
+              padding: const EdgeInsets.all(10.0),
+              width: double.infinity,
+              height: 180,
+              child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    controls = !controls;
+                    print(controls);
+                  });
+                },
+                child: ValueListenableBuilder<int>(
+                  valueListenable: WorkoutCounter.number,
+                  builder: (context, value, child) {
+                    return Text(
+                      WorkoutCounter.number.value.toString(),
+                      style: clickCount < 100
+                          ? const TextStyle(fontSize: 100)
+                          : clickCount < 1000
+                              ? const TextStyle(fontSize: 75)
+                              : const TextStyle(fontSize: 50),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+          Visibility(
+            visible: controls,
+            child: Container(
+              child: FloatingActionButton(
+                onPressed: () {},
+                child: Icon(Icons.remove),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
